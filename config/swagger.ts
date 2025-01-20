1a5fd39627ec20d6defb255ac5cc6bdaed8ed64a
Noah Manaigre <nmanaigre2@academic.rrc.ca>
2025-01-19 20:56:35 -0600

    Code cleanup

--- config/swagger.ts
+++ config/swagger.ts
@@ -14,11 +14,8 @@ const swaggerOptions: Options = {
 	},
 	apis: ["./src/app.ts"],
 };
-
 const swaggerDocs: any = swaggerJsDoc(swaggerOptions);
-
 const setupSwagger = (app: Express): void => {
 	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 };
-
 export default setupSwagger;
--- express-api/src/app.ts
+++ express-api/src/app.ts
@@ -2,10 +2,8 @@ import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
-// Middleware
 app.use(express.json());
 
-// Basic routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Express API!');
 });
